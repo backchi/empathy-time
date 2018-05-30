788ab52e46aca12e3a26a1f53853e54e3916f26c
uo_ou97 <ansrmarud@gmail.com>
2018-05-30 07:08:03 +0000

    modified nev-bar

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_05_27_104606) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
